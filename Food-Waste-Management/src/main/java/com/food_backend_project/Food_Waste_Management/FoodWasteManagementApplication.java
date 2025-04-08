12af00f17fc7d99739bd30a0948dcbb371e437f3
techysahil <sahilsinghrajput3108@gmail.com>
2025-04-08 12:43:23 +0530

    backend commit

---
+++ Food-Waste-Management/src/main/java/com/food_backend_project/Food_Waste_Management/FoodWasteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.food_backend_project.Food_Waste_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWasteManagementApplication.class, args);
+	}
+
+}
