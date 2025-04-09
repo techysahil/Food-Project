9a9bc132456b46befca6a2548e0071961f4ce2d7
priyayynka <priyankashinde065@gmail.com>
2025-04-09 20:07:00 +0530

    landing page

---
+++ food-frontend-project/src/Components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BannerImage from "../Assets/home-banner-img.jpg";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Hot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeding, chopping
+            & marinating, so you can cook a fresh food.
+          </p>
+          <button className="secondary-button">
+            Donate Now! <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
