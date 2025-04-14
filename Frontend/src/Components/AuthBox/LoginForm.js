542ca968545a18ed545c72fbce2aa39760ab01db
ShreyaNKumar <126964046+ShreyaNKumar@users.noreply.github.com>
2025-04-14 16:07:17 +0530

    Update LoginForm.js

--- Frontend/src/Components/AuthBox/LoginForm.js
+++ Frontend/src/Components/AuthBox/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // if using react-router
+import { useNavigate } from "react-router-dom"; 
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
